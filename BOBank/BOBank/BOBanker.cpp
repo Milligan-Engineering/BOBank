8d74d78e1d56e91bd3539c8f0d5132dad17b4667
Jeffrey Giesey <jjgiesey@milligan.edu>
2018-02-01 10:39:41 -0500

    Implemented menu with switch

--- BOBank/BOBank/BOBanker.cpp
+++ BOBank/BOBank/BOBanker.cpp
@@ -16,7 +16,7 @@ int main()
 	double secondsPerMinute = 60;
 
 
-	int i = 1;
+	int i = 0;
 	int numberPlayers = 0;
 	int value;
 
@@ -26,13 +26,15 @@ int main()
 
 	cout << "Welcome to the B&O Banker Assitant \n";
 
-	while((numberPlayers < 3) || (numberPlayers > 6))
+	while((numberPlayers < 3) || (numberPlayers > 6)) // Check for valid input. If not - keep asking
 	{
 		cout << "Enter number of players between 3 and 6. \n";
 		cin >> numberPlayers;
 	}
 		cout << "There will be " << numberPlayers << " players. \n";
-	while(i <= numberPlayers)
+	
+		
+	while(i < numberPlayers) // Get name for each player
 	{
 		cout << "What is the name of the player " << i << "? \n";
 		cin >> playerName[i];
@@ -40,14 +42,51 @@ int main()
 		i++;
 	}
 
+//  Get turn time 
 	cout << "Enter the desired minutes of turn time: ";
 	cin >> turnTimeMinutes;
 
 	cout << "Enter the desired seconds of turn time: ";
 	cin >> turnTimeSeconds;
 
-	double turnTimeCombined = turnTimeMinutes + turnTimeSeconds / secondsPerMinute;
+	double turnTimeCombined = turnTimeMinutes + turnTimeSeconds / secondsPerMinute; // Convert to fractional minutes
 	cout << "The turn time is " << turnTimeMinutes << " minutes and " << turnTimeSeconds << " seconds or " << turnTimeCombined << " minutes \n";
+
+	int currentPlayer = 0;
+	bool goodInput = false;
+	char option;
+
+// Menu for player actions
+	while (!goodInput)
+	{
+		cout << "What would " << playerName[currentPlayer] << " like to do? \n";
+		cout << "   'b' = buy shares\n";
+		cout << "   's' = sell shares\n";
+		cout << "   'o' = orphan share buyback \n";
+		cout << "   'p' = pass \n";
+		cin >> option;
+		switch (option)
+		{
+		case 'b':
+			cout << playerName[currentPlayer] << " will buy shares. \n";
+			goodInput = true;
+			break;
+		case 's':
+			cout << playerName[currentPlayer] << " will sell shares. \n";
+			goodInput = true;
+			break;
+		case 'o':
+			cout << playerName[currentPlayer] << " will buy orphaned shares. \n";
+			goodInput = true;
+			break;
+		case 'p':
+			cout << playerName[currentPlayer] << " will pass. \n";
+			goodInput = true;
+			break;
+		default:
+			cout << "Invalid option. Try agian. \n";
+		}
+	}
 
 	cout << "Enter character to continue \n";
 	cin >> value;
