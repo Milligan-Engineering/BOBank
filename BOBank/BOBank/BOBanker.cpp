5be025d1ee0b97a3eec30a42b9f7fb3262ef4abd
Jeffrey Giesey <jjgiesey@milligan.edu>
2018-02-09 10:24:18 -0500

    more bad code

--- BOBank/BOBank/BOBanker.cpp
+++ BOBank/BOBank/BOBanker.cpp
@@ -1,39 +1,8 @@
-// Filename: Test.cpp 
+// Filename: BOBanker.cpp 
 // Author: JJ Giesey
 //Email: jjgiesey@milligan.edu
-//Project: Quiz 09
-//Description: Example of function call
-//Last Modified: 02/09/18
-
-#include <iostream>
-#include <string>
-#include <stdlib.h> // need to include library for fundtion rand, srand
-#include <time.h>  // need to include library for fundtion time
-
-using namespace std;
-
-// Function Declarations
-int summer(int value1, int value2);
-//Precondition: The two values to be added are passed in value1 and value 2
-//Postcondition: The sum of these values is returned.
-
-
-int main()
-{
-	int a = 3, b=4;
-	int a,b,sum;
-
-	a = 3;
-	b = 4;
-	sum = summer(a, b)
-	cout << "a=" << a << " and b=" << b << " so sum is " << sum << endl;
-
-	return(0);
-}
-
-int summmer(int val1, int val2)
-{
-	int TheSum;
-	TheSumm = val1 + val2;
-	return(TheSum);
-}
+-//Project Milestone: 05 a
++//Project Milestone: 05 b
+ //Description: Program to handle banking duites in B&O Boardgame
+	-//Last Modified: 02/07/18
+	+//Last Modified: 02/08/18
